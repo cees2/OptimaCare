864d10ec2d13485500d690edfc9a8684d4e90c15
Damian Mirek <damianmirek1234@gmail.com>
2025-04-03 20:10:40 +0200

    Carousel section add

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
+enum ToggleNavItemMode {
+  MAKE_REGULAR,
+  MAKE_ACTIVE,
+}
+
 const servicesNavClickHandler = (event: MouseEvent) => {
   if (!event.target || !(event.target instanceof HTMLLIElement)) return;
 
@@ -37,4 +42,108 @@ const servicesSectionHandler = () => {
   servicesNav?.addEventListener("click", servicesNavClickHandler);
 };
 
+const toggleNavigationItem = (
+  navItem: Element | null,
+  toggleMode: ToggleNavItemMode
+) => {
+  if (toggleMode === ToggleNavItemMode.MAKE_ACTIVE) {
+    navItem?.classList.add("carousel__navigation-item--active");
+  } else {
+    navItem?.classList.remove("carousel__navigation-item--active");
+  }
+};
+
+const carouselArrowClickHandler = (
+  carousel: HTMLDivElement,
+  activeCarouselElement: number
+) => {
+  const carouselItemsContainer: HTMLDivElement | null =
+    carousel.querySelector(".carousel__items");
+  const previousActiveCarouselItem: HTMLDivElement | undefined | null =
+    carouselItemsContainer?.querySelector(".carousel__item--active");
+  const currentActiveCarouselItem =
+    carouselItemsContainer?.children[activeCarouselElement];
+  const navigationItems: NodeListOf<HTMLDivElement> | null =
+    carousel.querySelectorAll(".carousel__navigation-item");
+  const currentActiveNavigationItem = navigationItems[activeCarouselElement];
+  const previousActiveNavigationItem = carousel.querySelector(
+    ".carousel__navigation-item--active"
+  );
+
+  if (
+    !carouselItemsContainer ||
+    !previousActiveCarouselItem ||
+    !currentActiveCarouselItem
+  )
+    return;
+
+  carouselItemsContainer.style.transform = `translateX(-${
+    activeCarouselElement * 100
+  }%)`;
+
+  previousActiveCarouselItem.classList.remove("carousel__item--active");
+  currentActiveCarouselItem.classList.add("carousel__item--active");
+
+  toggleNavigationItem(
+    previousActiveNavigationItem,
+    ToggleNavItemMode.MAKE_REGULAR
+  );
+
+  toggleNavigationItem(
+    currentActiveNavigationItem,
+    ToggleNavItemMode.MAKE_ACTIVE
+  );
+};
+
+const carouselHandler = () => {
+  const carousels: NodeListOf<HTMLDivElement> | null =
+    document.querySelectorAll(".carousel");
+
+  if (!carousels) return;
+
+  carousels.forEach((carousel) => {
+    const currentCarouselItemContainer: HTMLDivElement | null =
+      carousel.querySelector(".carousel__items");
+    const currentCarouselArrowLeft = carousel.querySelector(
+      ".carousel__arrow--left"
+    );
+    const currentCarouselArrowRight = carousel.querySelector(
+      ".carousel__arrow--right"
+    );
+    const currentCarouselNavigationContainer: HTMLDivElement | null =
+      carousel.querySelector(".carousel__navigation");
+
+    if (
+      !currentCarouselItemContainer ||
+      !currentCarouselArrowLeft ||
+      !currentCarouselArrowRight ||
+      !currentCarouselNavigationContainer
+    )
+      return;
+
+    let activeCarouselElement: number = 0;
+    const {
+      children: { length: numberOfItems },
+    } = currentCarouselItemContainer;
+    const { children: navigationItems } = currentCarouselNavigationContainer;
+    const activeNavigationItem = navigationItems[activeCarouselElement];
+
+    currentCarouselItemContainer.style.width = `${numberOfItems * 100}%`;
+    toggleNavigationItem(activeNavigationItem, ToggleNavItemMode.MAKE_ACTIVE);
+
+    currentCarouselArrowLeft.addEventListener("click", () => {
+      activeCarouselElement--;
+      if (activeCarouselElement < 0) activeCarouselElement = numberOfItems - 1;
+      carouselArrowClickHandler(carousel, activeCarouselElement);
+    });
+
+    currentCarouselArrowRight.addEventListener("click", () => {
+      activeCarouselElement++;
+      if (activeCarouselElement === numberOfItems) activeCarouselElement = 0;
+      carouselArrowClickHandler(carousel, activeCarouselElement);
+    });
+  });
+};
+
 servicesSectionHandler();
+carouselHandler();
