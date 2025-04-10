b261696ebd1c5fea63eaf32746b4f3e976c7dea1
Damian Mirek <damianmirek1234@gmail.com>
2025-04-10 20:07:53 +0200

    form styles change & disclaimer modal add

--- scripts/modal.ts
+++ scripts/modal.ts
@@ -13,36 +13,53 @@ const modalClickHandler = (event: MouseEvent) => {
   }
 };
 
+const modalTriggerHandler = (event: MouseEvent) => {
+  if (
+    !(event.target instanceof HTMLElement) ||
+    !event.target.dataset.dropdownTriggerId
+  )
+    return;
+
+  const modalID = event.target.dataset.dropdownTriggerId;
+  const modalElement = document.getElementById(modalID);
+  const modalCloseIcon: HTMLDivElement | undefined | null =
+    modalElement?.querySelector(".modal__close-icon");
+
+  if (!modalElement || !modalCloseIcon) return;
+
+  modalElement.classList.add("active");
+  modalElement.addEventListener("click", modalClickHandler);
+  modalCloseIcon.addEventListener(
+    "click",
+    () => closeModalHandler(modalElement),
+    { once: true }
+  );
+};
+
 const addEventListenersToModalTriggers = () => {
   const modalTriggers: NodeListOf<HTMLElement> | null =
     document.querySelectorAll("[data-dropdown-trigger-id]");
-
-  const modalTriggerHandler = (event: MouseEvent) => {
-    if (
-      !(event.target instanceof HTMLElement) ||
-      !event.target.dataset.dropdownTriggerId
-    )
-      return;
-
-    const modalID = event.target.dataset.dropdownTriggerId;
-    const modalElement = document.getElementById(modalID);
-    const modalCloseIcon: HTMLDivElement | undefined | null =
-      modalElement?.querySelector(".modal__close-icon");
-
-    if (!modalElement || !modalCloseIcon) return;
-
-    modalElement.classList.add("active");
-    modalElement.addEventListener("click", modalClickHandler);
-    modalCloseIcon.addEventListener(
-      "click",
-      () => closeModalHandler(modalElement),
-      { once: true }
-    );
-  };
 
   modalTriggers?.forEach((modalTrigger) => {
     modalTrigger.addEventListener("click", modalTriggerHandler);
   });
 };
 
+const addEventListenersToDisclaimerModal = () => {
+  const displaimerModal: HTMLDivElement | null =
+    document.querySelector("#disclaimer-modal");
+  const modalCloseIcon: HTMLDivElement | undefined | null =
+    displaimerModal?.querySelector(".modal__close-icon");
+
+  if (!displaimerModal || !modalCloseIcon) return;
+
+  displaimerModal?.addEventListener("click", modalClickHandler);
+  modalCloseIcon?.addEventListener(
+    "click",
+    () => closeModalHandler(displaimerModal),
+    { once: true }
+  );
+};
+
 addEventListenersToModalTriggers();
+addEventListenersToDisclaimerModal();
