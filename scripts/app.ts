5737671be796acbec09cfb8a88ebdaa4bbe4ebd8
Damian Mirek <damianmirek1234@gmail.com>
2025-04-24 19:46:14 +0200

    detect active navigation item on page scroll

--- scripts/app.ts
+++ scripts/app.ts
@@ -6,6 +6,8 @@ const mainPageElementsExcludingFirstSection = [
   ...sectionsArray.slice(1),
   footer,
 ];
+const mainNavList: HTMLUListElement | null =
+  document.querySelector(".header__nav-list");
 
 const elementObserverHandler = (
   entries: IntersectionObserverEntry[],
@@ -13,9 +15,24 @@ const elementObserverHandler = (
 ) => {
   const [entry] = entries;
 
-  if (entry.isIntersecting) {
+  if (entry.isIntersecting && entry.intersectionRatio > 0.95) {
+    const activeMainNavigationTab = entry.target.getAttribute("id");
+
+    if (!activeMainNavigationTab) return;
+
+    const headerLinkToBeActive: HTMLAnchorElement | null =
+      document.querySelector(`a[href="#${activeMainNavigationTab}"]`);
+
+    if (!headerLinkToBeActive) return;
+
+    const { parentElement: mainNavListElement } = headerLinkToBeActive;
+    const currentActiveNavListElement: HTMLLIElement | null | undefined =
+      mainNavList?.querySelector(".navigation__item--active");
+
+    currentActiveNavListElement?.classList.remove("navigation__item--active");
+    mainNavListElement?.classList.add("navigation__item--active");
+  } else if (entry.isIntersecting) {
     entry.target.classList.add("content--active");
-    observer.unobserve(entry.target);
   }
 };
 
@@ -23,7 +40,7 @@ mainPageElementsExcludingFirstSection.map((element) => {
   if (!element) return;
 
   const observer = new IntersectionObserver(elementObserverHandler, {
-    threshold: 0.4,
+    threshold: [0.4, 0.95],
   });
 
   observer.observe(element);
